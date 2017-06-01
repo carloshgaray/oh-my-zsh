cf5ac558ad8dfdfc66cb0730700dc559cc3d4ba1
Carlos Garay <garac@amazon.com>
2017-06-02 00:21:56 +0100

    corrected install repo

--- tools/install.sh
+++ tools/install.sh
@@ -61,7 +61,7 @@ main() {
       exit 1
     fi
   fi
-  env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
+  env git clone --depth=1 https://github.com/carloshgaray/oh-my-zsh.git $ZSH || {
     printf "Error: git clone of oh-my-zsh repo failed\n"
     exit 1
   }
@@ -80,20 +80,20 @@ main() {
   " ~/.zshrc > ~/.zshrc-omztemp
   mv -f ~/.zshrc-omztemp ~/.zshrc
 
-  # If this user's login shell is not already "zsh", attempt to switch.
-  TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
-  if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
-    # If this platform provides a "chsh" command (not Cygwin), do it, man!
-    if hash chsh >/dev/null 2>&1; then
-      printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
-      chsh -s $(grep /zsh$ /etc/shells | tail -1)
-    # Else, suggest the user do so manually.
-    else
-      printf "I can't change your shell automatically because this system does not have chsh.\n"
-      printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
-    fi
-  fi
-
+  # # If this user's login shell is not already "zsh", attempt to switch.
+  # TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+  # if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+  #   # If this platform provides a "chsh" command (not Cygwin), do it, man!
+  #   if hash chsh >/dev/null 2>&1; then
+  #     printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
+  #     chsh -s $(grep /zsh$ /etc/shells | tail -1)
+  #   # Else, suggest the user do so manually.
+  #   else
+  #     printf "I can't change your shell automatically because this system does not have chsh.\n"
+  #     printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
+  #   fi
+  # fi
+  #
   # printf "${GREEN}"
   # echo '         __                                     __   '
   # echo '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
